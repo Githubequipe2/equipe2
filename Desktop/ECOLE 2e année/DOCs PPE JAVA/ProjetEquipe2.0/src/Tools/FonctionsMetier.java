0027279ffdc01e732a7c2a95e61e76f719b91e22
alexk <alex.koskas2@gmail.com>
2020-12-23 13:06:36 +0100

    AK 5e commit IMetier remplis

--- Desktop/ECOLE 2e année/DOCs PPE JAVA/ProjetEquipe2.0/src/Tools/FonctionsMetier.java
+++ Desktop/ECOLE 2e année/DOCs PPE JAVA/ProjetEquipe2.0/src/Tools/FonctionsMetier.java
@@ -5,11 +5,82 @@
  */
 package Tools;
 
+import Entity.Laboratoire;
+import Entity.Region;
+import Entity.Secteur;
+import Entity.Visiteur;
+import java.util.ArrayList;
+
 /**
  *
  * @author alexk
  */
 public class FonctionsMetier implements IMetier
 {
+
+    @Override
+    public void InsererRegion(int regCode, int secCode, String regNom) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int GetLastCodeRegion() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Secteur> GetAllSecteurs() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int GetCodeSecteur() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Region> GetAllRegions() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void ModifierRegion(String regNom) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String GetNomRegion() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void InsererVisiteur(int visMat, String visNom, String visPrenom, String visAdresse, String visCp, String visVille, String visDateEmbauche, int secCode, int laboCode) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int GetLastMatricule() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Laboratoire> GetAllLaboratoire() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Visiteur> GetAllVisiteur() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void ModifierVisiteur(String visNom, String visVille, String visAdresse, String visCp, int secCode, int laboCode) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void VisiteurInsererRegion(int visMatricule, String JJMMAA, int regCode, String traRole) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
--- Desktop/ECOLE 2e année/DOCs PPE JAVA/ProjetEquipe2.0/src/Tools/IMetier.java
+++ Desktop/ECOLE 2e année/DOCs PPE JAVA/ProjetEquipe2.0/src/Tools/IMetier.java
@@ -5,10 +5,35 @@
  */
 package Tools;
 
+import Entity.Laboratoire;
+import Entity.Region;
+import Entity.Secteur;
+import Entity.Visiteur;
+import java.util.ArrayList;
+
 /**
  *
  * @author alexk
  */
 public interface IMetier {
+    public void InsererRegion(int regCode, int secCode, String regNom);
+    public int GetLastCodeRegion();
+    public ArrayList<Secteur> GetAllSecteurs();
+    public int GetCodeSecteur();
+    
+    public ArrayList<Region> GetAllRegions();
+    public void ModifierRegion(String regNom);
+    public String GetNomRegion();
+    
+    public void InsererVisiteur(int visMat, String visNom, String visPrenom, String visAdresse, String visCp, String visVille, String visDateEmbauche, int secCode, int laboCode);
+    public int GetLastMatricule();
+    public ArrayList<Laboratoire> GetAllLaboratoire();
+    
+    public ArrayList<Visiteur> GetAllVisiteur();
+    public void ModifierVisiteur(String visNom, String visVille, String visAdresse, String visCp, int secCode, int laboCode);
+    
+    public void VisiteurInsererRegion(int visMatricule, String JJMMAA, int regCode, String traRole);
     
 }
+
+
